1e69d402deb82182a2b86a00a7d31af2cdd6d855
ajaykumar432 <127184830+ajaykumar432@users.noreply.github.com>
2026-05-31 17:03:26 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/scoreme_scheduler';
+  const uri = process.env.MONGODB_URL || 'mongodb://localhost:27017/scoreme_scheduler';
   await mongoose.connect(uri, {
     serverSelectionTimeoutMS: 5000,
   });
